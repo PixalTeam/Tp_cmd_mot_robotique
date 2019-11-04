f8e02e3e100fef58d24117b08ac42a4e96ca6cfb
Kellis <34947108+Gumbraise@users.noreply.github.com>
2019-11-04 12:55:33 +0100

    Add files via upload

---
+++ Tp_cmd_mot_robotique_LELOUER_FICE.ino
@@ -0,0 +1,19 @@
+#include <MotorShield.h>
+MS_DCMotor motor(MOTOR_A);
+void setup() {
+  motor.run(BRAKE);
+  motor.setSpeed(127.5);
+}
+
+void loop() {
+  motor.run(RELEASE);
+  motor.run(FORWARD);
+  delay(2000) ;
+  motor.run(BRAKE);
+  delay(1000) ;
+  motor.run(RELEASE);
+  motor.run(BACKWARD);
+  delay(2000) ;
+  motor.run(BRAKE);
+  delay(1000) ;
+}
---
+++ Tp_cmd_mot_robotique_LELOUER_FICE2.ino
@@ -0,0 +1,43 @@
+#include <MotorShield.h>
+MS_DCMotor motora(MOTOR_A);
+MS_DCMotor motorb(MOTOR_B);
+void setup() {
+  motora.run(BRAKE);
+  motora.setSpeed(255);
+  motorb.run(BRAKE);
+  motorb.setSpeed(255);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+}
+void loop() {
+  if (digitalRead(4) == '1') {
+    motora.run(RELEASE);
+    motorb.run(RELEASE);
+
+    motora.run(BACKWARD);
+    motorb.run(FORWARD);
+  }
+  if (digitalRead(5) == '1') {
+    motora.run(RELEASE);
+    motorb.run(RELEASE);
+
+    motora.run(FORWARD);
+    motorb.run(BACKWARD);
+  }
+  if (digitalRead(6) == '1') {
+    motora.run(RELEASE);
+    motorb.run(RELEASE);
+
+    motora.run(FORWARD);
+    motorb.run(FORWARD);
+  }
+  if (digitalRead(7) == '1') {
+    motora.run(RELEASE);
+    motorb.run(RELEASE);
+
+    motora.run(BACKWARD);
+    motorb.run(BACKWARD);
+  }
+}
