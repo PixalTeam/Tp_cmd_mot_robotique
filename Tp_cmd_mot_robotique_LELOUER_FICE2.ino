fa2a3a68038ef4ea396e3e963af93983311e63d8
Kellis <vikiclashonyt@gmail.com>
2019-11-18 12:57:00 +0100

    1811

--- Tp_cmd_mot_robotique_LELOUER_FICE2.ino
+++ Tp_cmd_mot_robotique_LELOUER_FICE2.ino
@@ -1,43 +1,47 @@
-#include <MotorShield.h>
-MS_DCMotor motora(MOTOR_A);
-MS_DCMotor motorb(MOTOR_B);
+#include <MotorShield.h> //INCLURE LA BIBLIOTHEQUE
+MS_DCMotor motora(MOTOR_A); //DETECTER LE MOTEUR A
+MS_DCMotor motorb(MOTOR_B); //DETECTER LE MOTEUR B
 void setup() {
-  motora.run(BRAKE);
-  motora.setSpeed(255);
-  motorb.run(BRAKE);
-  motorb.setSpeed(255);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
-  pinMode(7, INPUT);
+  motora.run(BRAKE); //COUPER LE MOTEUR A
+  motora.setSpeed(255); //METTRE LA VITESSE DU MOTEUR A AU MAX
+  motorb.run(BRAKE); //COUPER LE MOTEUR B
+  motorb.setSpeed(255); //METTRE LA VITESSE DU MOTEUR B AU MAX
+  pinMode(4, INPUT); //BOUTON GAUCHE RELIEE AU PIN 4
+  pinMode(5, INPUT); //BOUTON DROIT RELIEE AU PIN 5
+  pinMode(6, INPUT); //BOUTON AVANT RELIEE AU PIN 6
+  pinMode(7, INPUT); //BOUTON ARRIERE RELIEE AU PIN 7
 }
 void loop() {
-  if (digitalRead(4) == '1') {
-    motora.run(RELEASE);
-    motorb.run(RELEASE);
+  if (digitalRead(4) == 1) { //SI LE BOUTON GAUCHE EST POUSSE
+    motora.run(RELEASE); //LIBERER LE MOTEUR A
+    motorb.run(RELEASE); //LIBERER LE MOTEUR B
 
-    motora.run(BACKWARD);
-    motorb.run(FORWARD);
+    motora.run(BACKWARD); //FAIRE TOUENR LE MOTEUR A DANS LE SENS ANTIHORAIRE
+    motorb.run(FORWARD); //FAIRE TOUENR LE MOTEUR B DANS LE SENS HORAIRE
   }
-  if (digitalRead(5) == '1') {
-    motora.run(RELEASE);
-    motorb.run(RELEASE);
+  else if (digitalRead(5) == 1) { //SI LE BOUTON DROIT EST POUSSE
+    motora.run(RELEASE);//LIBERER LE MOTEUR A
+    motorb.run(RELEASE);//LIBERER LE MOTEUR B
 
-    motora.run(FORWARD);
-    motorb.run(BACKWARD);
+    motorb.run(BACKWARD); //FAIRE TOUENR LE MOTEUR A DANS LE SENS ANTIHORAIRE
+    motora.run(FORWARD); //FAIRE TOUENR LE MOTEUR B DANS LE SENS HORAIRE
   }
-  if (digitalRead(6) == '1') {
-    motora.run(RELEASE);
-    motorb.run(RELEASE);
+  else if (digitalRead(6) == 1) { //SI LE BOUTON AVANT EST POUSSE
+    motora.run(RELEASE);//LIBERER LE MOTEUR A
+    motorb.run(RELEASE);//LIBERER LE MOTEUR B
 
-    motora.run(FORWARD);
-    motorb.run(FORWARD);
+    motora.run(FORWARD); //FAIRE TOUENR LE MOTEUR B DANS LE SENS HORAIRE
+    motorb.run(FORWARD); //FAIRE TOUENR LE MOTEUR B DANS LE SENS HORAIRE
   }
-  if (digitalRead(7) == '1') {
-    motora.run(RELEASE);
-    motorb.run(RELEASE);
+  else if (digitalRead(7) == 1) { //SI LE BOUTON ARRIERE EST POUSSE
+    motora.run(RELEASE);//LIBERER LE MOTEUR A
+    motorb.run(RELEASE);//LIBERER LE MOTEUR B
 
-    motora.run(BACKWARD);
-    motorb.run(BACKWARD);
+    motora.run(BACKWARD); //FAIRE TOUENR LE MOTEUR A DANS LE SENS ANTIHORAIRE
+    motorb.run(BACKWARD); //FAIRE TOUENR LE MOTEUR A DANS LE SENS ANTIHORAIRE
+  }
+  else {
+    motora.run(BRAKE); //COUPER LE MOTEUR A
+    motorb.run(BRAKE); //COUPER LE MOTEUR B
   }
 }
